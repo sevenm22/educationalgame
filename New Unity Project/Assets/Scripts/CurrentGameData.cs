30b949b4aef31092194981f1b19a201f64078bd6
sevenm27 <480630@chrome.escg.ac.uk>
2019-11-14 16:29:39 +0000

    Added play button and coded it and also added data.

---
+++ New Unity Project/Assets/Scripts/CurrentGameData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentGameData : MonoBehaviour
+{
+    [HideInInspector]
+    public int FirstFlagIndex = 0;
+    public int SecondFlagIndex;
+    public int FinalFlagIndex;
+
+    private int PrevFinalFlagIndex;
+    private int CountriesPergame = 60; //How many countries should be in one game.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PickCountriesForGame()
+    {
+        int PickedCountryNumber = 0;
+    }
+
+}
+
---
+++ New Unity Project/Assets/Scripts/GameData.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    [System.Serializable]
+
+    public struct CountryData
+    {
+        public string Name;
+        public Sprite Flag;
+        public bool Guessed;
+        public bool BeenQuestioned;
+    }
+
+    public CountryData[] EuropeanCountryDataSet;
+    public CountryData[] AsiaCountryDataSet;
+    public CountryData[] AfricaCountryDataSet;
+    [HideInInspector]
+    public CountryData[] CountrySetPerGame;
+    [HideInInspector]
+    public CountryData[] CountryDataSet;
+
+    public static GameData Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(this);
+            Instance = this;
+        }
+        else
+            Destroy(this);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AssignArrayOfCountry()
+    {
+        CountryDataSet = new CountryData[EuropeanCountryDataSet.Length];
+        EuropeanCountryDataSet.CopyTo(CountryDataSet, 0);
+
+    }
+
+}
